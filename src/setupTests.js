55088efa2483f6aabc184c37458e94ea01e5e48f
Eemeli Aro <eemeli@gmail.com>
2022-02-18 16:02:07 +0200

    Use ts-jest for tests, dropping Babel config as unused

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,5 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
+const { configure } = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
 
 // Configure enzyme to work with Jest.
 configure({ adapter: new Adapter() });
