118e52e93fb898e157dfb62a9728395cbb5e0b4e
Eemeli Aro <eemeli@gmail.com>
2022-12-10 15:05:46 +0200

    Add getRules()

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { createMarker as default } from './createMarker';
+export { getRules } from './getRules';
 export { mark } from './mark';
 
 export type TagFunction = (input: string) => React.ReactElement<any>;
